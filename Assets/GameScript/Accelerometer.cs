72c4ca90dcc4422f1b5c0aa7ff55376856eb7f01
FarizAlfairuz <farizalfairuz27@gmail.com>
2021-05-14 10:11:39 +0700

    kura2 ninja

---
+++ Assets/GameScript/Accelerometer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    private float speed = 0.05f;
+    void Update()
+    {
+        transform.Translate(Input.acceleration.x * speed, 0, 0);
+    }
+}
