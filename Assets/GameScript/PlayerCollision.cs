881065c3c05fc9f09108fd8defdb237792884325
Bia <bia.irmal12@gmail.com>
2021-06-02 08:40:01 +0700

    power up effect by bia

--- Assets/GameScript/PlayerCollision.cs
+++ Assets/GameScript/PlayerCollision.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     //public PlayerMovement movement;
+    public float powerUpDuration = 1f;
+    public bool powerUpIsActivated = false;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag == "Obstacles")
+        if (collision.collider.tag == "Obstacles" && powerUpIsActivated == false)
         {
             //movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
         }
+        if (collision.gameObject.CompareTag("PowerUp"))
+        {
+            StartCoroutine(Pickup(collision.gameObject));
+        }
+    }
+
+    IEnumerator Pickup(GameObject obj)
+    {
+        Debug.Log("Power up is activated");
+        //Instantiate(pickupEffect, transform.position, transform.rotation);
+        Destroy(obj);
+        powerUpIsActivated = true;
+
+        yield return new WaitForSeconds(powerUpDuration);
+
+        powerUpIsActivated = false;
+        Debug.Log("Power up is deactivated");
     }
 }
